8b8beef1f54c46daaf8334e28402930bdd7cfbc0
TEST 1 <ellingtonc700@gmail.com>
2025-01-09 20:03:05 -0500

    ellington1/9/25 Establish Swervedrive.

--- src/main/java/frc/robot/subsystem/SwerveDrive.java
+++ src/main/java/frc/robot/subsystem/SwerveDrive.java
@@ -1,13 +1,52 @@
 package frc.robot.subsystem;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveDrive extends SubsystemBase {
+    public SwerveModule SwervemoduleFL;
+    public SwerveModule SwervemoduleFR;
+    public SwerveModule SwervemoduleBL;
+    public SwerveModule SwervemoduleBR;
+    public SwerveDriveKinematics kinematics;
 
     public static class Inputs {
         double initialRotation;
         double currentRotation;
     }
+public SwerveDrive(
+        SwerveModule SwerveModuleFL,
+        SwerveModule SwerveModuleFR,
+        SwerveModule SwerveModuleBR,
+        SwerveModule SwerveModuleBL){
+        this.SwervemoduleBR = SwerveModuleBR;
+    this.SwervemoduleBL = SwerveModuleBL;
+    this.SwervemoduleFR = SwerveModuleFR;
+    this.SwervemoduleFL = SwerveModuleFL;
+    kinematics = new SwerveDriveKinematics(
+            new Translation2d(SwerveModuleFL.xpos, SwerveModuleFL.ypos ),
+            new Translation2d(SwerveModuleFR.xpos, SwerveModuleFR.ypos ),
+            new Translation2d(SwerveModuleBR.xpos, SwerveModuleBR.ypos ),
+            new Translation2d(SwerveModuleBL.xpos, SwerveModuleBL.ypos )
+    );
+}
+public void Drive(double xpower, double ypower, double turnspeed
+
+) {
+   ChassisSpeeds Cspeeds = new ChassisSpeeds(xpower, ypower, turnspeed);
+   SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(Cspeeds);
+   SwervemoduleFL.drive(moduleStates[0].speedMetersPerSecond);
+   SwervemoduleFL.pivot(moduleStates[0].angle.getMeasure());
+    SwervemoduleFL.drive(moduleStates[1].speedMetersPerSecond);
+    SwervemoduleFR.pivot(moduleStates[1].angle.getMeasure());
+    SwervemoduleBR.drive(moduleStates[2].speedMetersPerSecond);
+    SwervemoduleBR.pivot(moduleStates[2].angle.getMeasure());
+    SwervemoduleBL.drive(moduleStates[3].speedMetersPerSecond);
+    SwervemoduleBL.pivot(moduleStates[3].angle.getMeasure());
 
 
+}
 }
--- src/main/java/frc/robot/subsystem/SwerveModule.java
+++ src/main/java/frc/robot/subsystem/SwerveModule.java
@@ -1,10 +1,51 @@
 package frc.robot.subsystem;
 
-public class SwerveModule {
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.units.measure.Angle;
 
+import static edu.wpi.first.units.Units.Degrees;
+
+public class SwerveModule {
+public SparkMax drivemotor;
+    public SparkMax pivmotor;
+    public PIDController pivotcontroller;
+    public CANcoder compass;
+    public double xpos;
+    public double ypos;
+    public double heading;
     public static class Inputs {
         public double getangle;
         public double pivpower;
         public double drivepower;
+
+    }
+    public SwerveModule(
+            SparkMax drivemotor,
+            SparkMax pivmotor,
+            CANcoder compass,
+            double xpos ,
+            double ypos,
+            double heading
+
+    ){
+        this.compass = compass;
+        this.pivmotor = pivmotor;
+        this. drivemotor = drivemotor;
+        this.pivotcontroller = new PIDController(0.001,0.001,0);
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.heading = heading;
+
+    }
+    public void drive(double power){
+        drivemotor.set(power);
+    }
+    public void pivot(Angle targetHeading){
+        Angle currentHeading = compass.getAbsolutePosition().getValue();
+        Angle error = currentHeading.minus(targetHeading);
+        double pivpower = pivotcontroller.calculate(error.in(Degrees));
+        pivmotor.set(pivpower);
     }
 }
