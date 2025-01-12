2017f6afdd7ff5ef20952f4b6ff203402a393489
TEST 1 <ellingtonc700@gmail.com>
2025-01-12 16:57:35 -0500

    ellington1/12/25 Establish Odometry and Kinematics.

--- src/main/java/frc/robot/subsystem/SwerveModule.java
+++ src/main/java/frc/robot/subsystem/SwerveModule.java
@@ -3,49 +3,73 @@ package frc.robot.subsystem;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
 
 import static edu.wpi.first.units.Units.Degrees;
 
 public class SwerveModule {
-public SparkMax drivemotor;
+    public SparkMax drivemotor;
     public SparkMax pivmotor;
     public PIDController pivotcontroller;
     public CANcoder compass;
     public double xpos;
     public double ypos;
     public double heading;
+
     public static class Inputs {
         public double getangle;
         public double pivpower;
         public double drivepower;
 
     }
+
     public SwerveModule(
             SparkMax drivemotor,
             SparkMax pivmotor,
             CANcoder compass,
-            double xpos ,
+            double xpos,
             double ypos,
             double heading
 
-    ){
+    ) {
         this.compass = compass;
         this.pivmotor = pivmotor;
-        this. drivemotor = drivemotor;
-        this.pivotcontroller = new PIDController(0.001,0.001,0);
+        this.drivemotor = drivemotor;
+        this.pivotcontroller = new PIDController(0.001, 0.001, 0);
         this.xpos = xpos;
         this.ypos = ypos;
         this.heading = heading;
 
     }
-    public void drive(double power){
+
+    public void drive(double power) {
         drivemotor.set(power);
     }
-    public void pivot(Angle targetHeading){
+
+    public void pivot(Angle targetHeading) {
         Angle currentHeading = compass.getAbsolutePosition().getValue();
         Angle error = currentHeading.minus(targetHeading);
         double pivpower = pivotcontroller.calculate(error.in(Degrees));
         pivmotor.set(pivpower);
+    }
+
+    public SwerveModuleState getmodulestate() {
+        SwerveModuleState moduleState = new SwerveModuleState(
+                drivemotor.getEncoder().getVelocity(),
+                new Rotation2d(compass.getAbsolutePosition().getValue())
+        );
+    }
+
+    public SwerveModulePosition getModulePosition() {
+        SwerveModulePosition position = new SwerveModulePosition(
+                Units.Inches.of(drivemotor.getEncoder().getPosition()).in(Units.Meters),
+                new Rotation2d(compass.getAbsolutePosition().getValue())
+        );
+        return position;
+
     }
 }
