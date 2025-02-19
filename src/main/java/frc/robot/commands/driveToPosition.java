81edfe59c8f75a1224dc9d958c44dd40d23c9542
Adole <anuradhabdole@gmail.com>
2025-02-19 18:18:36 -0500

    Drive to position command created on Mon, Feb 17. It uses the generate trajectory imported command to create a path from a current position to a target position.

---
+++ src/main/java/frc/robot/commands/driveToPosition.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystem.LimeLights;
+import frc.robot.subsystem.LimelightHelpers;
+import frc.robot.subsystem.SwerveDrive;
+import edu.wpi.first.math.geometry.*;
+
+import java.util.ArrayList;
+
+public class driveToPosition extends Command {
+    // Figures out current location and desired location
+    // Drives to that new location
+
+    private final SwerveDrive swerveDrive;
+    private Pose2d targetPose;
+    private Pose2d currentPose;
+
+    public driveToPosition(SwerveDrive swerveDrive, Pose2d targetPose, Pose2d currentPose) {
+        this.swerveDrive = swerveDrive;
+        this.currentPose = currentPose;
+    }
+
+    @Override
+    public void initialize() {
+        // targetPose = targetPose.transformBy(currentPose.minus(targetPose));
+        currentPose = LimelightHelpers.getBotPose2d("");
+    }
+
+    public void execute() {
+        ArrayList<Translation2d> internalPoints = new ArrayList<Translation2d>();
+
+        swerveDrive.generateTrajectory(currentPose, targetPose, internalPoints, 0, 0);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
+
